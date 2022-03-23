734a582c5681d1b18479b1f80f1ad27c73640e27
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-03-23 17:45:04 +0700

    delete email

--- src/main/java/com/example/backendmd6/repository/ProfileEnterpriseRepository.java
+++ src/main/java/com/example/backendmd6/repository/ProfileEnterpriseRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfileEnterpriseRepository extends JpaRepository<ProfileEnterprise, Long> {
+    ProfileEnterprise findByEmail(String email);
 
     void delete(ProfileEnterprise entity);
 
--- src/main/java/com/example/backendmd6/repository/ProfileUserRepository.java
+++ src/main/java/com/example/backendmd6/repository/ProfileUserRepository.java
@@ -8,5 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfileUserRepository extends JpaRepository<ProfileUser, Long> {
 
+    ProfileUser findByEmail(String email);
+
     void delete(ProfileUser entity);
 }
